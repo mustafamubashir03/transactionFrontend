6b992d2128c7763a1bc8d2e1b87cc2ef6431cb56
Mustafa Mubashir <114705378+mustafamubashir03@users.noreply.github.com>
2024-12-12 14:07:03 +0500

    Update UserTab.jsx

--- components/UserTab.jsx
+++ components/UserTab.jsx
@@ -10,11 +10,10 @@ export default function UserTab({ letter, firstName, lastName,id,balance}) {
         <ProfileIcon letter={letter} />
         <p className="self-center">{firstName} {lastName}</p>
       </div>
-      <div className="flex space-between gap-2">
-        <p className="bg-green-100 px-2 py-1 rounded-full border-2 border-green-700 text-green-700 font-semibold text-sm">${balance}</p>
+        <p className="bg-green-100 px-1 rounded-full border-2 border-green-700 text-green-700 font-semibold text-xs">${balance}</p>
       <Link to={`/send?to=${id}&firstName=${firstName}&lastName=${lastName}`}>
         <ButtonSmall label={"Send money"} />
-      </Link></div>
+      </Link>
     </div>
   );
 }
