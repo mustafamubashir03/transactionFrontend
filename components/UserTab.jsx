774ce40fc76e9f9b4e5f476679c70ff90753f979
Mustafa Mubashir <114705378+mustafamubashir03@users.noreply.github.com>
2024-12-12 14:24:01 +0500

    Update UserTab.jsx

--- components/UserTab.jsx
+++ components/UserTab.jsx
@@ -6,11 +6,12 @@ export default function UserTab({ letter, firstName, lastName,id,balance}) {
 
   return (
     <div className="border-2 rounded-md px-4 flex justify-between mt-4 items-center">
+      <div className="grid grid-rows-2 gap-2">
       <div className=" flex  gap-2">
         <ProfileIcon letter={letter} />
         <p className="self-center">{firstName} {lastName}</p>
-        <p className="bg-green-100 px-1 rounded-full border-2 border-green-700 text-green-700 font-semibold text-xs">${balance}</p>
       </div>
+        <p className="bg-green-100 px-1 rounded-full border-2 border-green-700 text-green-700 font-semibold text-xs">${balance}</p></div>
       <Link to={`/send?to=${id}&firstName=${firstName}&lastName=${lastName}`}>
         <ButtonSmall label={"Send money"} />
       </Link>
