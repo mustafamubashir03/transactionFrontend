80699652a9382379fcd69fc2d7bd221f289a0c9b
Mustafa Mubashir <114705378+mustafamubashir03@users.noreply.github.com>
2024-12-12 14:32:06 +0500

    Update UserTab.jsx

--- components/UserTab.jsx
+++ components/UserTab.jsx
@@ -12,7 +12,7 @@ export default function UserTab({ letter, firstName, lastName,id,balance}) {
         <p className="self-center">{firstName} {lastName}</p>
       </div>
         <p className="bg-green-100 px-1 rounded-full border-2 border-green-700 text-green-700 font-semibold text-sm flex
-          items-center justify-center">${balance}</p></div>
+          items-center justify-center max-w-30">${balance}</p></div>
       <Link to={`/send?to=${id}&firstName=${firstName}&lastName=${lastName}`}>
         <ButtonSmall label={"Send money"} />
       </Link>
